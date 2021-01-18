c9d0e5642f4bfb4a5a6a34c19a513ccbfd93c2b5
hugoleite72 <hugoita14@outlook.com>
2021-01-18 00:23:32 -0200

    Mudanca nos arquivos html css e js

--- funcaojs.js
+++ funcaojs.js
@@ -11,6 +11,32 @@ switchText.addEventListener("click", function(){
     }  
 }, false);  
 
+"use strict";
+
+var switchText = document.getElementById("faq1");  
+var elToggled = document.getElementById("lad1");  
+elToggled.style.display = "none";  
+switchText.addEventListener("click", function(){  
+    if(elToggled.style.display == "none") {  
+        elToggled.style.display = "block";  
+    } else {  
+        elToggled.style.display = "none";  
+    }  
+}, false);  
+
+
+"use strict";
+
+var switchText = document.getElementById("faq2");  
+var elToggled = document.getElementById("lad2");  
+elToggled.style.display = "none";  
+switchText.addEventListener("click", function(){  
+    if(elToggled.style.display == "none") {  
+        elToggled.style.display = "block";  
+    } else {  
+        elToggled.style.display = "none";  
+    }  
+}, false);  
 
 
 
